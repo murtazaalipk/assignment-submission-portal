bd5969d707c634755cf10a72a635dffa337ce878
Muhammad Anas Sohail <115977114+muhammadanascode@users.noreply.github.com>
2024-06-30 17:31:44 +0500

    color and description of task has been changed

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,7 +1,8 @@
-export default function Task() {
+export default function Task({ message }) {
     return (
-        <div className="bg-gray-200 w-64 h-32 flex justify-center items-center">
-            <h2 className="text-gray-800 text-lg font-semibold">Task Title</h2>
+        <div className="bg-blue-200 w-64 h-32 flex justify-center items-center">
+            {console.log(message)}
+            <h2 className="text-gray-800 text-lg font-semibold">{message}</h2>
         </div>
     );
 }
