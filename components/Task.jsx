df450af34b7a4deb88897d8be3cd75ebc8736ff5
Muhammad Anas Sohail <115977114+muhammadanascode@users.noreply.github.com>
2024-06-30 18:44:18 +0500

    console removed

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,7 +1,6 @@
 export default function Task({ message }) {
     return (
         <div className="bg-blue-200 w-64 h-32 flex justify-center items-center">
-            {console.log(message)}
             <h2 className="text-gray-800 text-lg font-semibold">{message}</h2>
         </div>
     );
