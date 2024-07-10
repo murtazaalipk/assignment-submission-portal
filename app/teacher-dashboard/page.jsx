d7fb6223e4182f7ab17e4a33f0a4469d2cc3c562
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2024-07-11 03:01:47 +0500

    Update page.jsx

--- app/teacher-dashboard/page.jsx
+++ app/teacher-dashboard/page.jsx
@@ -1,8 +1,8 @@
 "use client";
-
 import Cart from "@/components/Cart";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+
 export default function TeacherDashboard() {
     const { data: session } = useSession();
 
@@ -12,11 +12,17 @@ export default function TeacherDashboard() {
                 Hey Sir {session?.user?.name}, Welcome Back
             </h1>
             <div className="flex flex-wrap justify-center gap-4">
-                <Cart course={"FLutter"} batch={"1"}  city={"Karachi"}  days={"Wed & Fri"}  status={true} />
-                <Cart course={"FLutter"} batch={"2"}  city={"Karachi"}  days={"Sat & Sun"}  status={false}/>
-                <Cart course={"FLutter"} batch={"3"}  city={"Karachi"}  days={"Mon & Tue"}  status={false}/>
-            
+                <Link href={{ pathname: "/class-dashboard", query: { course: "Flutter", batch: "1", city: "Karachi", days: "Wed & Fri", status: true } }} passHref>
+                    <Cart course={"Flutter"} batch={"1"} city={"Karachi"} days={"Wed & Fri"} status={true} />
+                </Link>
+                <Link href={{ pathname: "/class-dashboard", query: { course: "Flutter", batch: "2", city: "Karachi", days: "Sat & Sun", status: false } }} passHref>
+                    <Cart course={"Flutter"} batch={"2"} city={"Karachi"} days={"Sat & Sun"} status={false} />
+                </Link>
+                <Link href={{ pathname: "/class-dashboard", query: { course: "Flutter", batch: "3", city: "Karachi", days: "Mon & Tue", status: false } }} passHref>
+                    <Cart course={"Flutter"} batch={"3"} city={"Karachi"} days={"Mon & Tue"} status={false} />
+                </Link>
             </div>
         </>
     );
 }
+
