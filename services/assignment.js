487e0549032815876b512e0f6987338ce15431ba
muhammadanascode <anassohal34343@gmail.com>
2024-07-13 15:37:12 +0500

    finding class by classId and updating its assignment array with new assignment posted

--- services/assignment.js
+++ services/assignment.js
@@ -1,7 +1,10 @@
+import Class from "../models/classes";
 import Assignment from "../models/assignments";
 
 /**
+ * 
  * Creates a new assignment document in the database.
+ * @param {mongoose.Types.ObjectId} params.classId - The ID of the class the assignment belongs to.
  * @param {string} title - The title of the assignment.
  * @param {string} description - The description or instructions for the assignment.
  * @param {Date} dueDate - The due date of the assignment.
@@ -9,6 +12,7 @@ import Assignment from "../models/assignments";
  * @throws {Error} - If there's an error while saving the assignment.
  */
 export const createAssignment = async ({
+  classId,
   title,
   description,
   dueDate,
@@ -25,6 +29,18 @@ export const createAssignment = async ({
 
     // Save the new assignment to the database and await the operation
     const assignment = await newAssignment.save();
+
+    // Find the existing class by ID and add the assignment reference to the assignments array
+    const existingClass = await Class.findByIdAndUpdate(
+      classId,
+      { $push: { assignments: assignment._id } },
+      { new: true }
+    );
+
+    // Check if the class was found and updated
+    if (!existingClass) {
+      throw new Error("Class not found");
+    }
 
     // Return the saved assignment object
     return assignment;
