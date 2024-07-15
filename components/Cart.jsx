9cc1155a5aa62f5d3fa111b03b244c49e512eb4e
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2024-07-15 23:17:40 +0500

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -2,7 +2,7 @@ export default function Cart({course,batch,city,days,status}) {
 const completeClass = "bg-[#def1f7] text-blue-800 font-bold";
 const inProgressClass = "bg-green-200 text-green-800";
     return (
-        <main className=" font-signika cursor-pointer w-80 p-4 flex flex-col justify-center items-start rounded-lg shadow-md hover:shadow-lg hover:bg-[#def1f7] transition-all">
+        <main className=" font-signika cursor-pointer w-[360px] p-4 flex flex-col justify-center items-start rounded-lg shadow-md hover:shadow-lg hover:bg-[#def1f7] transition-all">
             <h1 className=" font-[500] text-[16px] ">{course}</h1>
             <div className="flex gap-10 py-4 text-[16px]">
                 <div className="flex flex-col"> 
