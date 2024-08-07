d327e565d55b5ba0411d3a8bf106997cf1b75585
Muhammad Zohaib <syedzohaib7658@gmail.com>
2024-08-07 20:21:36 +0500

    Create Admin panel

---
+++ app/admin/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TeacherAdminPanel from "@/components/TeacherAdminPanel";
+
+
+
+
+export default async function Admin() {
+  
+    return <TeacherAdminPanel />;
+  };
