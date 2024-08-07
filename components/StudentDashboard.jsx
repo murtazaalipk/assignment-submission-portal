d68ca954d81130a9e39be75465984dacb635fee1
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2024-08-07 18:06:38 +0500

    Update StudentDashboard.jsx

--- components/StudentDashboard.jsx
+++ components/StudentDashboard.jsx
@@ -7,75 +7,14 @@ import Link from "next/link";
 export default function StudentDashboard() {
   const { data: session } = useSession();
   const [courses, setCourses] = useState([]);
+  const email = session?.user?.email
 
   // Simulate fetching data from a database
   useEffect(() => {
     const fetchCourses = async () => {
-      // Simulated fetched data
-      const fetchedCourses = [
-        {
-          id: 1,
-          course: "Flutter",
-          batch: "1",
-          city: "Karachi",
-          days: "Wed & Fri",
-          status: true,
-          assignments: [
-            {
-              name: "Assignment 1",
-              dueDate: "2024-07-20",
-              Status: "pending",
-            },
-            {
-              name: "Assignment 2",
-              dueDate: "2024-07-27",
-              Status: "complete",
-            },
-          ],
-        },
-        {
-          id: 2,
-          course: "Flutter",
-          batch: "2",
-          city: "Karachi",
-          days: "Sat & Sun",
-          status: false,
-          assignments: [
-            {
-              name: "Assignment 1",
-              dueDate: "2024-07-22",
-              Status: "pending",
-            },
-            {
-              name: "Assignment 2",
-              dueDate: "2024-07-29",
-              Status: "complete",
-            },
-          ],
-        },
-        {
-          id: 3,
-          course: "Flutter",
-          batch: "3",
-          city: "Karachi",
-          days: "Mon & Tue",
-          status: false,
-          assignments: [
-            {
-              name: "Assignment 1",
-              dueDate: "2024-07-25",
-              Status: "pending",
-            },
-            {
-              name: "Assignment 2",
-              dueDate: "2024-08-01",
-              Status: "complete",
-            },
-          ],
-        },
-      ];
-      setCourses(fetchedCourses);
-    };
+    const fetchedCourses = await (await fetch(`http://localhost:3000/api/classes?email=${email}`)).json()
+    setCourses(fetchedCourses.classes)
+  };
 
     fetchCourses();
   }, []);
@@ -87,7 +26,7 @@ export default function StudentDashboard() {
       </h1>
       <div className="flex flex-wrap justify-center gap-4">
         {courses.map((course) => (
-          <Link key={course.id} href={`/class-dashboard/${course.id}`} passHref>
+          <Link key={course.id} href={`/class-dashboard/${course._id}`} passHref>
             <Cart {...course} />
           </Link>
         ))}
