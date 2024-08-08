fdf425bdd710817b8c4e7cf5b5b08bc82b4a89f9
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2024-08-08 14:44:49 +0500

    Update AddClass.jsx

--- components/AddClass.jsx
+++ components/AddClass.jsx
@@ -14,6 +14,19 @@ const AddClass = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handleCheckboxChange = (e) => {
+    const { value, checked } = e.target;
+    let selectedDays = formData.days ? formData.days.split(' & ') : [];
+
+    if (checked) {
+      selectedDays.push(value);
+    } else {
+      selectedDays = selectedDays.filter(day => day !== value);
+    }
+
+    setFormData({ ...formData, days: selectedDays.join(' & ') });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch('http://localhost:3000/api/classes', {
@@ -24,10 +37,10 @@ const AddClass = () => {
       body: JSON.stringify(formData),
     });
     const data = await response.json();
-    if (data.success) {
-        alert('API call was successful!');
-        console.log(data); // Handle the data from the API here
-    }
+    if (data.success) { 
+      alert('Class Created successful!');
+      console.log(data); // Handle the data from the API here
+      }
   };
 
   return (
@@ -51,8 +64,22 @@ const AddClass = () => {
           <input className="w-full p-2 border" type="text" id="city" name="city" value={formData.city} onChange={handleChange} />
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium mb-1" htmlFor="days">Days</label>
-          <input className="w-full p-2 border" type="text" id="days" name="days" value={formData.days} onChange={handleChange} />
+          <label className="block text-sm font-medium mb-1">Days</label>
+          <div className="">
+            {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(day => (
+              <div key={day} className="flex items-center">
+                <input
+                  className="mr-1"
+                  type="checkbox"
+                  id={day}
+                  name="days"
+                  value={day}
+                  onChange={handleCheckboxChange}
+                />
+                <label htmlFor={day}>{day}</label>
+              </div>
+            ))}
+          </div>
         </div>
         <button className="bg-blue-500 text-white py-2 px-4 rounded" type="submit">Add Class</button>
       </form>
